20d712f857586e3821515bc7e0ffd92f202423c0
MichelleHaase <michelle-haase@gmx.de>
2025-02-15 16:11:57 +0100

    schema for db created

--- passwordmanager.py
+++ passwordmanager.py
@@ -8,6 +8,32 @@ Core features:
 4. tkinter UI
     4.1 maybe categories like social media banks or similar
     4.2 search function for categories (tables?) or websites etc
-5. maybe other option on saving websites, notes etc'''
+5. maybe other option on saving websites, notes etc
 
-def main():
+plan:
+1. create datbase
+2. create'''
+
+import graphics
+
+def main() -> None:
+    ...
+
+
+def insert_Data() -> None:
+    ...
+
+
+def hashing(input) -> str:
+    ...
+
+
+def retrieve_Data() -> None:
+    ...
+
+
+
+
+
+if __name__ == "__main__":
+    main()
